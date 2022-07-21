d2ed464d5f29d6204df806054d4d172fd96097ee
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-07-21 21:19:13 +0900

    feat: OPアニメーションを実装

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,32 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        themeGray: "#475156",
+        clay: "#e8e7df",
+      },
+      animation: {
+        slideInOpLogo: "slideIn 0.7s ease-in forwards",
+        slideInOpText: "slideIn 0.7s ease-in 1.0s forwards",
+        slideInOpSubText: "slideIn 0.7s ease-in 1.2s forwards",
+      },
+      keyframes: {
+        slideIn: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(15px)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
