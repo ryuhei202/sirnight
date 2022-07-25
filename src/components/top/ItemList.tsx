ff3cbef893ec19ff5a974feab7ef905b030bf585
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-07-25 18:43:45 +0900

    feat: アイテムの画像を追加

---
+++ src/components/top/ItemList.tsx
@@ -0,0 +1,44 @@
+type TProps = {
+  type: "tops" | "bottoms";
+};
+
+export const ItemList = ({ type }: TProps) => {
+  return (
+    <div className="w-[200%] flex first:mb-7">
+      <ul
+        className={`w-[50%] flex bg-clay ${
+          type === "tops"
+            ? "animate-infiniteSlideLeft"
+            : "animate-infiniteSlideRight"
+        }`}
+      >
+        {[...Array(type === "tops" ? 3 : 4)].map((_, index) => (
+          <li className="mx-2">
+            <img
+              src={`/images/items/${type}/${index + 1}.svg`}
+              alt={`items-${type}-${index + 1}`}
+              key={index}
+            />
+          </li>
+        ))}
+      </ul>
+      <ul
+        className={`w-[50%] flex bg-clay ${
+          type === "tops"
+            ? "animate-infiniteSlideLeft"
+            : "animate-infiniteSlideRight"
+        }`}
+      >
+        {[...Array(type === "tops" ? 3 : 4)].map((_, index) => (
+          <li className="mx-2">
+            <img
+              src={`/images/items/${type}/${index + 1}.svg`}
+              alt={`items-${type}-${index + 1}`}
+              key={index}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
