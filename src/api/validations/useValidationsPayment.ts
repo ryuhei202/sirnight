6f89a67987ec40245d094dfe15f84be916c3a065
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-08-02 16:34:30 +0900

    feat: 支払い情報のバリデーションapi hooksを実装

---
+++ src/api/validations/useValidationsPayment.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from "axios";
+import { UseMutateFunction } from "react-query";
+import { usePostRequest } from "../usePostRequest";
+
+type TValidationsPayment = {
+  readonly mutate: UseMutateFunction<
+    AxiosResponse,
+    unknown,
+    TValidationsPaymentParams,
+    unknown
+  >;
+  readonly isLoading: boolean;
+};
+
+export type TValidationsPaymentParams = {
+  readonly cardToken: string;
+  readonly isAgree: boolean;
+  readonly serialCode?: string;
+};
+
+export const useValidationsPayment = (): TValidationsPayment => {
+  const { mutate, isLoading } = usePostRequest<TValidationsPaymentParams>(
+    "validations/payment"
+  );
+
+  return { mutate, isLoading };
+};
