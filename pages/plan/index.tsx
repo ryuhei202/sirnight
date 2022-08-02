73509a7161bf3ef5d527d189ce2cad95cdfa366f
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-08-02 15:40:01 +0900

    fix: 不要な文字を削除

--- pages/plan/index.tsx
+++ pages/plan/index.tsx
@@ -13,21 +13,29 @@ const Plan: NextPage = () => {
         />
         <link rel="icon" href="/favicon/favicon_head.png" />
       </Head>
-      <div id="container" className="h-full bg-clay sm:w-[500px] inset-0 text-themeGray">
+      <div
+        id="container"
+        className="h-full bg-clay sm:w-[500px] inset-0 text-themeGray"
+      >
         <div className="relative w-full p-2 text-[#979B9A]">
-          <Link href="/">トップ</Link><span> > 料金プラン</span>
+          <Link href="/">トップ</Link>
+          <span>料金プラン</span>
         </div>
         <h2 className="text-3xl my-10 text-center font-bold">料金プラン</h2>
         <div>
           <div className="flex flex-row justify-between px-4">
-            <span className="w-1/3 text-center py-4 mx-2 border-b-2 border-themeGray">ライト</span>
-            <span className="w-1/3 text-center py-4 mx-2 border-b-2 border-themeGray">スタンダード</span>
-            <span className="w-1/3 text-center py-4 mx-2 border-b-2 border-themeGray">プレミアム</span>
+            <span className="w-1/3 text-center py-4 mx-2 border-b-2 border-themeGray">
+              ライト
+            </span>
+            <span className="w-1/3 text-center py-4 mx-2 border-b-2 border-themeGray">
+              スタンダード
+            </span>
+            <span className="w-1/3 text-center py-4 mx-2 border-b-2 border-themeGray">
+              プレミアム
+            </span>
           </div>
           <div>
-            <div className="">
-
-            </div>
+            <div className=""></div>
           </div>
         </div>
       </div>
