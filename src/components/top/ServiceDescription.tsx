892dbcd80d8b7e4d234d108daa5509fb0ff6f28c
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-08-12 12:04:37 +0900

    style: 文字の大きさを調整

--- src/components/top/ServiceDescription.tsx
+++ src/components/top/ServiceDescription.tsx
@@ -1,12 +1,12 @@
 export const ServiceDescription = () => {
   return (
     <div className="relative p-10 text-center bg-clay" id="service-description">
-      <h3 className="text-xl tracking-wider leading-9 font-semibold">
+      <h3 className="text-[6vw] sm:text-3xl mt-7 tracking-wider leading-10 font-semibold">
         UWearは、あなたと一緒に
         <br />
         最適なコーデを考えます。
       </h3>
-      <p className="text-sm mt-8 leading-6">
+      <p className="sm:text-xl mt-14 mb-10 leading-[2.5rem]">
         UWear(ユーウェア)は、
         <br />
         洋服の悩みをスタイリストが解決し、
