8a4c00151d090138185c21adc247bae2449339a0
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-08-16 12:21:08 +0900

    fix: 環境変数が指定されていなかったので修正

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
 
 const sentryWebpackPluginOptions = {
   silent: true,
+  authTolen: process.env.SENTRY_AUTH_TOKEN,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
 };
 
 module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
