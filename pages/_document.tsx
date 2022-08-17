b496526e9602ebd4bd595b29b8d87c8b2b42f619
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-08-17 19:52:59 +0900

    fix: Next.jsのScriptタグを使用しないように修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 import { GA_ID } from "../src/lib/gtag";
 type Props = {};
 
@@ -18,21 +17,21 @@ class Document extends NextDocument<Props> {
             <></>
           ) : (
             <>
-              <Script
-                defer
+              <script
+                async
                 src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-                strategy="afterInteractive"
               />
-              <Script id="Google Analitics" defer strategy="afterInteractive">
-                {`
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', '${GA_ID}', {
                     page_path: window.location.pathname,
-                  });
-                `}
-              </Script>
+                  });`,
+                }}
+              />
             </>
           )}
         </Head>
