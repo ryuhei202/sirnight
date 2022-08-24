155cd4ddccebc9042829d7f3ab92627b8d4d858e
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-08-24 15:27:02 +0900

    style: 文字の大きさを調整

--- src/components/top/CoordinateCard.tsx
+++ src/components/top/CoordinateCard.tsx
@@ -37,20 +37,22 @@ export const CoordinateCard = ({
               className="object-cover object-top h-[calc(100vh-70px)] w-full"
             />
           </figure>
-          <div className="h-fit bg-clay z-30 mt-[25vh] -mb-1 [transform:translate3d(0,0,0)]">
-            <div className="flex flex-row w-full text-sm border-y border-themeGray font-semibold">
-              <h2 className="bg-themeGray text-clay w-1/4 text-center py-2.5">
+          <div className="h-fit z-30 mt-[25vh] -mb-1 [transform:translate3d(0,0,0)]">
+            <div className="flex flex-row w-full text-[2.5vw] sm:text-sm border-y border-themeGray font-semibold">
+              <p className="bg-themeGray text-clay w-1/4 text-[3vw] sm:text-sm text-center py-2.5">
                 {coordinateNumber}
-              </h2>
-              <p className="w-3/4 px-5 py-2.5">{title}</p>
+              </p>
+              <p className="w-3/4 bg-clay text-[3vw] sm:text-sm px-5 py-2.5">
+                {title}
+              </p>
             </div>
-            <div className="px-6 py-6">
-              <p className="text-xl">
-                <span className="border-b-2 border-dashed border-themeGray">
+            <div className="p-[5vw] sm:p-7 bg-clay">
+              <p className="text-[4vw] sm:text-xl leading-[8vw] sm:leading-10 font-semibold">
+                <span className="pb-1 border-b-[1.5px] sm:border-b-2 border-dashed border-themeGray">
                   {mainText}
                 </span>
               </p>
-              <p className="text-sm mt-5">{subText}</p>
+              <p className="text-[3vw] sm:text-sm mt-5">{subText}</p>
             </div>
           </div>
         </div>
