a3b3b05f98e989f70f5833f00bb6c2a6dffe3bc7
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-08-30 19:42:37 +0900

    fix: スマホで見ると行間が広かったので修正

--- src/components/top/ServiceDescription.tsx
+++ src/components/top/ServiceDescription.tsx
@@ -4,12 +4,12 @@ export const ServiceDescription = () => {
       className="relative py-14 text-center bg-clay [font-feature-settings:'palt'on]"
       id="about"
     >
-      <h3 className="font-hiragino600 text-[6vw] sm:text-3xl leading-10 font-semibold">
+      <h3 className="font-hiragino600 text-[6vw] sm:text-3xl sm:!leading-[3rem] font-semibold">
         UWearは､あなたと一緒に
         <br />
         最適なコーデを考えます。
       </h3>
-      <p className="font-hiragino400 text-[max(3vw,0.85em)] sm:text-[1.125rem] mt-10 leading-9">
+      <p className="font-hiragino400 text-[max(3vw,0.85em)] sm:text-[1.125rem] mt-10 leading-[8vw] sm:leading-9">
         UWear（ユーウェア）は、
         <br />
         洋服の悩みをスタイリストが解決し、
