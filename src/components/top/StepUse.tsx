5c25bb9a6924ecde1f6794fe998d083cfa82beae
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-08-31 19:50:32 +0900

    style: カルーセルをiosに対応させる

--- src/components/top/StepUse.tsx
+++ src/components/top/StepUse.tsx
@@ -4,18 +4,23 @@ type TProps = {
   number: number;
   imagePath: string;
   title: ReactNode;
+  className?: string;
 };
 
-export const StepUse = ({ number, imagePath, title }: TProps) => {
+export const StepUse = ({ number, imagePath, title, className }: TProps) => {
   return (
-    <li className="flex-none w-full snap-always snap-center text-center relative mx-auto">
-      <p className="font-lora font-semibold">STEP 0{number}</p>
-      <p className="mx-auto mt-0.5 w-[5%] h-[1px] border-b border-dashed border-themeGray"></p>
-      <p className="mt-5 font-semibold text-[3.7vw] sm:text-xl">{title}</p>
+    <li className={`flex-none w-fit snap-always snap-center p-3 ${className}`}>
+      <div className="w-[80%] text-center mx-auto">
+        <span className="font-lora font-semibold">STEP 0{number}</span>
+        <hr className="w-[5%] mx-auto mt-0.5 h-[1px] border-b border-dashed border-white"></hr>
+        <span className="mt-5 font-semibold text-[3.7vw] sm:text-xl">
+          {title}
+        </span>
+      </div>
       <img
         src={imagePath}
         alt={`howToUse-${number}`}
-        className="rounded-[15px] mt-7 sm:h-fit block mx-auto"
+        className="rounded-[15px] mt-7 block mx-auto w-[80vw]"
       />
     </li>
   );
