6438ac46ea7b536b7fda783389415ae7f0f46b1e
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-09-01 02:55:52 +0900

    fix: marginを調整

--- src/components/top/StepUse.tsx
+++ src/components/top/StepUse.tsx
@@ -12,8 +12,8 @@ export const StepUse = ({ number, imagePath, title, className }: TProps) => {
     <li className={`flex-none w-fit snap-always snap-center p-3 ${className}`}>
       <div className="w-[80%] text-center mx-auto">
         <span className="font-lora font-semibold">STEP 0{number}</span>
-        <hr className="w-[5%] mx-auto mt-0.5 h-[1px] border-b border-dashed border-white"></hr>
-        <span className="mt-5 font-semibold text-[3.7vw] sm:text-xl">
+        <hr className="w-[5%] mx-auto mt-1 mb-5 h-[1px] border-b border-dashed border-white"></hr>
+        <span className="font-semibold text-[3.7vw] sm:text-xl">
           {title}
         </span>
       </div>
