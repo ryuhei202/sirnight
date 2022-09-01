2816893af53a28792e87d4584b6f1daf78697008
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-09-01 12:59:46 +0900

    feat: Thanksコンポーネント追加

---
+++ src/components/register/Affiliate.tsx
@@ -0,0 +1,25 @@
+type TProps = {
+  readonly memberId: number;
+};
+
+const Affiliate = ({ memberId }: TProps) => {
+  return (
+    <>
+      {/* もしもアフィリエイトコンバージョンタグ */}
+      <script
+        src={`https://r.moshimo.com/af/r/result.js?p_id=1063&pc_id=1537&m_v=${memberId}`}
+        id="msmaf"
+      ></script>
+      <noscript>
+        <img
+          src={`https://r.moshimo.com/af/r/result?p_id=1063&pc_id=1537&m_v=${memberId}`}
+          width="1"
+          height="1"
+          alt=""
+        />
+      </noscript>
+      {/* もしもアフィリエイトコンバージョンタグ */}
+    </>
+  );
+};
+export default Affiliate;
