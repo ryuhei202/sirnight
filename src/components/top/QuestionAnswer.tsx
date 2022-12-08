6333472b0e234591063c789061aadc16f39cf3f6
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2022-12-08 11:45:10 +0900

    feat:faqページのアンカーリンク設置 <LEEAP-3469-faqの各質問にアンカーリンクが設置されるようにする>

--- src/components/top/QuestionAnswer.tsx
+++ src/components/top/QuestionAnswer.tsx
@@ -1,26 +1,38 @@
+import Link from "next/link";
 import { useState } from "react";
-
 type TProps = {
+  contentId?: string;
   question: string;
   answer: string;
 };
 
-export const QuestionAnswer = ({ question, answer }: TProps) => {
+export const QuestionAnswer = ({ contentId, question, answer }: TProps) => {
   const [isActive, setIsActive] = useState(false);
   const handleClick = () => setIsActive((prevState) => !prevState);
   return (
-    <div className="border-t-2 border-solid border-[#D8D8D2] text-sm">
-      <div
-        className="flex flex-row justify-between cursor-pointer py-8 px-[10%]"
-        onClick={handleClick}
+    <div
+      className="border-t-2 border-solid border-[#D8D8D2] text-sm"
+      id={contentId}
+    >
+      <Link
+        href={{
+          hash: contentId,
+        }}
+        scroll={false}
       >
-        <p>{question}</p>
-        <img
-          src="/images/icons/arrow.svg"
-          alt="arrow-icon"
-          className={isActive ? "rotate-180" : ""}
-        />
-      </div>
+        <div
+          className="flex flex-row justify-between cursor-pointer py-8 px-[10%]"
+          onClick={handleClick}
+        >
+          <p>{question}</p>
+          <img
+            src="/images/icons/arrow.svg"
+            alt="arrow-icon"
+            className={isActive ? "rotate-180" : ""}
+          />
+        </div>
+      </Link>
+
       <div
         className={`pb-8 px-[10%] leading-6	font-normal ${
           isActive ? "mt-4" : "hidden"
