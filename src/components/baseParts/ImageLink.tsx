9f2aa633afc1e01ea1cefbf9dd98c74d8df01a52
Onishi_Kengo <k-ounishi@kiizan-kiizan.co.jp>
2023-01-13 18:12:39 +0900

    fix:next-linkに変更 <release/LEEAP-3630-アンカーリンクをfaqのページ上で取得できるようにする>

--- src/components/baseParts/ImageLink.tsx
+++ src/components/baseParts/ImageLink.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 type TProps = {
   link: string;
 };
 export const ImageLink = ({ link }: TProps) => {
   return (
-    <a href={link}>
+    <Link href={link}>
       <img
         src="/images/icons/link.svg"
         alt="copy-icon"
         className="w-8 h-8 hover:opacity-40"
       />
-    </a>
+    </Link>
   );
 };
