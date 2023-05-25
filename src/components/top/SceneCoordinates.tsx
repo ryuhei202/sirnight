642d0629d9439366aeb07a47e4f494323167ab55
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2023-05-25 19:02:04 +0800

    feat: コーデ例画像を春夏コーデの画像に差し替え <release/LEEAP-4436-hp写真を差し替える>

--- src/components/top/SceneCoordinates.tsx
+++ src/components/top/SceneCoordinates.tsx
@@ -11,7 +11,7 @@ export const SceneCoordinates = () => {
     <div className="h-fit" id="coordinates">
       <CoordinateCard
         imageFilePath="/images/coordinates/1.webp"
-        title="彼女と野外イベントへ"
+        title="彼女とライブイベントへ"
         coordinateNumber="コーデ#01"
         mainText={
           <>
@@ -20,13 +20,13 @@ export const SceneCoordinates = () => {
             明るく元気な印象を与えるカラーで
           </>
         }
-        subText="野外イベントのシーンに合わせて動きやすさを重視して、薄手のニットとチノパンのカジュアルコーデにしました。
+        subText="イベントのシーンに合わせて動きやすさを重視して、シャリ感のあるシャツにTシャツとチノパンを合わせたカジュアルコーデにしました。
         トップスに暖色カラーを取り入れて顔周りに色物を合わせることで明るく元気な印象に見えます。
         足元に白のスニーカーを合わせることでより明るさをプラスして、コーデに抜け感を出してくれます！"
       />
       <CoordinateCard
         imageFilePath="/images/coordinates/2.webp"
-        title="同僚とオフィスで休憩"
+        title="同僚とオフィスで打ち合わせ"
         coordinateNumber="コーデ#02"
         mainText={
           <>
@@ -35,9 +35,9 @@ export const SceneCoordinates = () => {
             オフィスワークコーデ
           </>
         }
-        subText="社内のお仕事シーンに合わせて、カーディガンを取り入れることで温度調整ができるキレイめカジュアルコーデにしました。
-        カーディガンをハイゲージ素材にすることできちんとさも残しつつ、襟のないバンドカラーシャツでほどよいリラックスのある印象に見えます。
-        トップスとボトムスの色にメリハリを出すことで、まとまりが出てお仕事シーンにぴったりのコーデです！"
+        subText="仕事で同僚と会議をするシーンに合わせて、ジャケットを使ったビジネスカジュアルコーデにしました。
+        襟のないトップスで程よくカジュアルさを取り入れています。ネイビーのジャケットの寒色系とトップスのホワイトで顔回りを明るくした清潔感のある爽やかなコーデにしました。
+        ボトムスにはベージュで明るい色合いなので、靴にはブラックを合わせるとコーデの色合が締りきちんと感のあるスタイルにしています。"
       />
       <CoordinateCard
         imageFilePath="/images/coordinates/3.webp"
@@ -70,29 +70,13 @@ export const SceneCoordinates = () => {
               少し遠出で楽しい気分に合わせて、グリーンやブルーなどの色物を取り入れることで明るく、爽やかな印象に見えます。
               カットソーだけではラフすぎるので、シャツを合わせることできちんと感をプラスした、幅広い旅行先に合うコーデにしました。"
       />
-      <CoordinateCard
-        imageFilePath="/images/coordinates/5.webp"
-        title="家族と休日のショッピング"
-        coordinateNumber="コーデ#05"
-        mainText={
-          <>
-            アースカラーとゆったりシルエットで
-            <br />
-            優しい印象のお父さんに
-          </>
-        }
-        subText="車でショッピングへ行くシーンに合わせて、動きやすさを意識したカジュアルコーデにしました。
-        アースカラーのボーダー柄は、色のメリハリを抑え柄物でも柔らかく優しい印象に見えます。
-        ゆったりとしたシルエットで動きやすさを取り入れて、暖かみのある素材のコーデュロイを選ぶことで季節感も出しました。
-        足元はスニーカーで合わせることで運転中も快適に過ごせるコーデにしました。"
-      />
       {isOpenMore ? (
         <>
           <div className="animate-fadeIn">
             <CoordinateCard
-              imageFilePath="/images/coordinates/6.webp"
+              imageFilePath="/images/coordinates/5.webp"
               title="家族で公園におでかけ"
-              coordinateNumber="コーデ#06"
+              coordinateNumber="コーデ#05"
               mainText={
                 <>
                   色味があるトップスで
@@ -100,14 +84,13 @@ export const SceneCoordinates = () => {
                   子供に負けない元気な印象に
                 </>
               }
-              subText="公園のシーンに合うように、レジャーシートを広げてのんびり過ごしたり、アクティブにも遊べる動きやすいカジュアルなコーデにしました。
-              色物を使うことで、表情を明るく見せることができるので、元気な印象に見えます。
-              上下ともにゆったりとしたシルエットにすることで、程よいラフ感が出て、動きやすく身軽な雰囲気に見えるコーデにしました。"
+              // TODO: 本文の修正
+              subText="TODO"
             />
             <CoordinateCard
-              imageFilePath="/images/coordinates/7.webp"
+              imageFilePath="/images/coordinates/6.webp"
               title="同僚と飲みに"
-              coordinateNumber="コーデ#07"
+              coordinateNumber="コーデ#06"
               mainText={
                 <>
                   気兼ねなく長時間でも
@@ -120,9 +103,9 @@ export const SceneCoordinates = () => {
               足元はスニーカーでカジュアルさを出しています。"
             />
             <CoordinateCard
-              imageFilePath="/images/coordinates/8.webp"
+              imageFilePath="/images/coordinates/7.webp"
               title="お客様と商談"
-              coordinateNumber="コーデ#08"
+              coordinateNumber="コーデ#07"
               mainText={
                 <>
                   全体をダークにキッチリ感をだしつつ、
@@ -135,9 +118,9 @@ export const SceneCoordinates = () => {
               インナーに明るい色を合わせることで顔周りが華やかに見えて、表情がよく見えるコーデにしました。"
             />
             <CoordinateCard
-              imageFilePath="/images/coordinates/9.webp"
-              title="恋人と高級レストランで記念日ディナー"
-              coordinateNumber="コーデ#09"
+              imageFilePath="/images/coordinates/8.webp"
+              title="彼女とお休みにカフェへ"
+              coordinateNumber="コーデ#08"
               mainText={
                 <>
                   ジャケットとタートルニットで
@@ -145,15 +128,14 @@ export const SceneCoordinates = () => {
                   いつもよりも大人な印象に
                 </>
               }
-              subText="記念日ディナーで少し高級感のあるお店のシーンに合わせて、ジャケットを取り入れたきれいめコーデにしました。
-              ジャケットを着ることできちんと感を出し、インナーにタートルニットを合わせることで大人っぽさを出しました。
-              全体的に色のトーンを暗めにまとめることで落ち着いた印象に見えます。
-              小物類は黒にして、足元を革靴で合わせることでより引き締まり上品さをプラスしてくれます。"
+              subText="カフェへ食事へ行くシーンに合わせて、リラックス感のあるカジュアルなコーデにしました。
+              おしゃれな雰囲気のカフェには、アースカラーを中心に落ち着いた色でまとめて、シンプルなTシャツにブルゾンを合わせてアクセントを効かせたコーデにしました。
+              靴はブラックにすることで、リラックス感のあるコーデでも全体の色味が締って見えます。"
             />
             <CoordinateCard
-              imageFilePath="/images/coordinates/10.webp"
+              imageFilePath="/images/coordinates/9.webp"
               title="彼女とショッピングへ"
-              coordinateNumber="コーデ#10"
+              coordinateNumber="コーデ#9"
               mainText={
                 <>
                   温かみを感じる素材感とダークカラーで
@@ -170,9 +152,9 @@ export const SceneCoordinates = () => {
       ) : (
         <div className="relative">
           <CoordinateCard
-            imageFilePath="/images/coordinates/6.webp"
+            imageFilePath="/images/coordinates/5.webp"
             title="家族で公園におでかけ"
-            coordinateNumber="コーデ#06"
+            coordinateNumber="コーデ#05"
             mainText={
               <>
                 色味があるトップスで
