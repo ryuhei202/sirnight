2790c112e6c71b3d15063cee61807407db3d30b6
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-08-17 16:11:41 +0900

    feat: UWearについてのプレビュー画面の実装 <release/LEEAP-4624-microcms「uwearについて」を書いている途中でも> <(no>  <(no>

--- pages/about/[id].tsx
+++ pages/about/[id].tsx
@@ -1,9 +1,5 @@
 import { GetStaticProps, NextPage } from "next";
-import { NextSeo } from "next-seo";
-import Link from "next/link";
-import { NewsContent } from "../../src/components/news/NewsContent";
-import { Header } from "../../src/components/plan/Header";
-import { FooterMenu } from "../../src/components/top/FooterMenu";
+import { AboutContentWrapper } from "../../src/components/about/AboutContentWrapper";
 import {
   TAbout,
   TAboutContent,
@@ -42,57 +38,9 @@ type TProps = {
 const AboutContent: NextPage<TProps> = ({ article, aboutData }: TProps) => {
   return (
     <div className="h-full">
-      <NextSeo
-        title={`${article.title}`}
-        openGraph={{
-          images: [
-            {
-              url: article.image
-                ? article.image.url
-                : `https://uwear.jp/images/meta/ogp.jpg`,
-              width: 1200,
-              height: 630,
-              alt: article.title,
-            },
-          ],
-        }}
-      />
-      <div
-        id="container"
-        className="h-full min-h-screen inset-0 text-themeGray"
-      >
-        <Header lists={[{ name: `${article.title}` }]} />
-        <img src={article.image.url} alt={article.title} className="w-full" />
-        <div className="px-4">
-          <div className="w-full py-12 border-b-[1px] border-[#D8D8D2]">
-            <h2 className="pt-2 text-xl font-bold">{`${article.title}`}</h2>
-          </div>
-          <div className="py-12 text-sm">
-            <NewsContent content={article?.content} />
-          </div>
-        </div>
-        <div className="pb-24">
-          <h3 className="border-b-[1px] border-[#D8D8D2] mx-4 text-2xl">
-            関連記事
-          </h3>
-          <div className="flex w-full">
-            {aboutData.contents.map((data) => (
-              <Link href={`/about/${data.id}`} key={data.id}>
-                <div className="w-[50%] p-4 border-x-[1px] border-[#D8D8D2]">
-                  <img
-                    src={data.image.url}
-                    alt={data.title}
-                    className="w-full h-[30vw] sm:h-[150px] mx-auto object-cover"
-                  />
-                  <p className="text-sm mt-2">{data.title}</p>
-                </div>
-              </Link>
-            ))}
-          </div>
-        </div>
-        <FooterMenu />
-      </div>
+      <AboutContentWrapper article={article} aboutData={aboutData} />
     </div>
   );
 };
+
 export default AboutContent;
