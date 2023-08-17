ab6815b7203ca01100733acdf763b5b4e04d2c93
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-08-17 18:01:50 +0900

    fix: 日本時刻として表示するよう修正 <bugfix/LEEAP-4498-microcmsで1日公開時間がずれる> <(no>

--- src/lib/microCMS/handleDate.ts
+++ src/lib/microCMS/handleDate.ts
@@ -4,8 +4,10 @@ type TProps = {
 };
 
 export const handleDate = ({ date, type = "dot" }: TProps) => {
+  const d = new Date(date);
+  const japanDateTime = new Date(d.setHours(d.getHours() + 9)).toISOString();
   const regex = new RegExp("T.+Z", "g");
-  const dateTimeRemoved = date.replace(regex, "");
+  const dateTimeRemoved = japanDateTime.replace(regex, "");
 
   const formattedDate =
     type === "dot"
