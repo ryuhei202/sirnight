6c6c494f12fd0b303b20ecd9c7e001a316853b4f
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-08-21 16:48:41 +0900

    chore: Headerではなく、Breadcrumbに名前を変更 <release/LEEAP-4734-sirnight記事ページをレスポンシブ対応させる>

--- src/components/top/FooterMenu.tsx
+++ src/components/top/FooterMenu.tsx
@@ -24,39 +24,40 @@ export const FooterMenu = () => {
     }).then(() => router.push({ pathname: "/plan" }));
   };
 
-  return <>
-    <Menu onClick={onClickIcon} footerState={footerState} />
-    <div className="fixed bottom-0 h-[70px] sm:w-[498px] bg-clay flex z-40 border-t border-solid border-[#475156]">
-      <Link
-        href={{ pathname: "/" }}
-        className="h-full w-1/4 border-r border-solid border-[#475156] flex justify-center items-center">
-
-        <img src="/images/logos/gray.svg" alt="logo" className="w-[77%]" />
-
-      </Link>
-      <div
-        className="h-full w-1/2 flex justify-center items-center cursor-pointer"
-        onClick={onClickIcon}
-      >
-        <button className="w-[26%]">
-          {footerState === "open" ? (
-            <img src="/images/icons/one-line.svg" alt="one-line" />
-          ) : (
-            <img
-              src="/images/icons/hamburger-menu.svg"
-              alt="hamberger-menu"
-            />
-          )}
-        </button>
+  return (
+    <>
+      <Menu onClick={onClickIcon} footerState={footerState} />
+      <div className="fixed bottom-0 h-[70px] sm:w-[498px] bg-clay flex z-40 border-t border-solid border-[#475156]">
+        <Link
+          href={{ pathname: "/" }}
+          className="h-full w-1/4 border-r border-solid border-[#475156] flex justify-center items-center"
+        >
+          <img src="/images/logos/gray.svg" alt="logo" className="w-[77%]" />
+        </Link>
+        <div
+          className="h-full w-1/2 flex justify-center items-center cursor-pointer"
+          onClick={onClickIcon}
+        >
+          <button className="w-[26%]">
+            {footerState === "open" ? (
+              <img src="/images/icons/one-line.svg" alt="one-line" />
+            ) : (
+              <img
+                src="/images/icons/hamburger-menu.svg"
+                alt="hamberger-menu"
+              />
+            )}
+          </button>
+        </div>
+        <a
+          onClick={handleClickPlan}
+          className="h-full w-1/4 bg-themeGray text-clay text-[3.5vw] sm:text-sm flex justify-center items-center cursor-pointer"
+        >
+          <span className="w-[77%] text-center [font-feature-settings:'palt'on]">
+            料金プラン
+          </span>
+        </a>
       </div>
-      <a
-        onClick={handleClickPlan}
-        className="h-full w-1/4 bg-themeGray text-clay text-[3.5vw] sm:text-sm flex justify-center items-center cursor-pointer"
-      >
-        <span className="w-[77%] text-center [font-feature-settings:'palt'on]">
-          料金プラン
-        </span>
-      </a>
-    </div>
-  </>;
+    </>
+  );
 };
