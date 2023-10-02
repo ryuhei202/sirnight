9abd71142d490af647dbf6e8154dfeeda3fefdbc
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-10-02 15:29:21 +0900

    remove: キャンペーン期間で使っていたコードを削除 <release/LEEAP-4876-hpのトップスの枚数を対応する>

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,14 +80,6 @@ const Home: NextPage<TProps> = ({
     <>
       {isOpeningVisible && <OpeningPage className="z-50" />}
       <div id="container" className="relative text-themeGray z-40">
-        {/* 夏の初月半額キャンペーン */}
-        {/* TODO: キャンペーン期間が過ぎたら削除 */}
-        {new Date() < new Date(2023, 9, 1) &&
-          new Date() >= new Date(2023, 5, 26) && (
-            <div className="sticky top-0 z-50 text-center bg-red font-lora w-full text-clay py-1">
-              9/30まで初月半額キャンペーン中！！
-            </div>
-          )}
         <KeyVisual />
         <CatchCopy />
         <ServiceDescription />
--- src/components/plan/PlanDetail.tsx
+++ src/components/plan/PlanDetail.tsx
@@ -46,160 +46,67 @@ export const PlanDetail = ({ plan, disabled }: TProps) => {
       </div>
       <div className="border-solid border border-themeGray rounded-md mx-3">
         <div>
-          {new Date() < new Date(2023, 9, 1) &&
-          new Date() >= new Date(2023, 5, 26) ? (
-            <>
-              {/* 夏の初月半額キャンペーン */}
-              {/* TODO: キャンペーン期間が過ぎたら削除 */}
-              <div className="text-center bg-red font-lora w-full text-clay py-1">
-                9/30まで初月半額キャンペーン中！！
-              </div>
-              <p className="text-center mt-6 mb-2 leading-none line-through font-lora">
-                月額{`¥${plan.price.withoutTax.toLocaleString()}`}（税別）
-                <br />
-                <span className="line-through">{`¥${plan.price.withTax.toLocaleString()}（税込）`}</span>
-              </p>
-              <p className="text-center mb-6 leading-none text-red">
-                月額
-                <span className="text-[10vw] sm:text-4xl ml-2 font-lora font-bold">{`¥${(
-                  plan.price.withoutTax / 2
-                ).toLocaleString()}`}</span>
-                （税別）
-                <br />
-                {`¥${(plan.price.withTax / 2).toLocaleString()}（税込）`}
-              </p>
-            </>
-          ) : (
-            <>
-              <p className="text-center mt-6 mb-2 leading-none">
-                月額
-                <span className="text-[10vw] sm:text-4xl ml-2 font-lora font-bold">{`¥${plan.price.withoutTax.toLocaleString()}`}</span>
-                （税別）
-              </p>
-              <div className="flex flex-wrap justify-center space-x-3 mb-6">
-                <div className="text-[4vw] sm:text-xl ">
-                  <span>{`¥${plan.price.withTax.toLocaleString()}（税込）`}</span>
-                </div>
-              </div>
-            </>
-          )}
+          <p className="text-center mt-6 mb-2 leading-none">
+            月額
+            <span className="text-[10vw] sm:text-4xl ml-2 font-lora font-bold">{`¥${plan.price.withoutTax.toLocaleString()}`}</span>
+            （税別）
+          </p>
+          <div className="flex flex-wrap justify-center space-x-3 mb-6">
+            <div className="text-[4vw] sm:text-xl ">
+              <span>{`¥${plan.price.withTax.toLocaleString()}（税込）`}</span>
+            </div>
+          </div>
         </div>
         <div className="bg-themeGray text-clay text-center text-[4vw] sm:text-xl py-1">
           コーデ数
         </div>
         <div className="h-full font-medium flex flex-col justify-evenly">
-          {new Date() < new Date(2023, 9, 1) &&
-          new Date() >= new Date(2023, 5, 26) &&
-          (plan.enName === "premium" || plan.enName === "standard") ? (
-            <>
-              {/* 夏のスタンダード・プレミアムアイテム１点追加キャンペーン */}
-              {/* TODO: キャンペーン期間が過ぎたら削除 */}
-              <div className="text-center bg-red font-lora w-full text-clay py-1">
-                9/30までトップス１点追加キャンペーン中！！
-              </div>
-              <div className="mx-auto py-4">
-                <p className="text-center mb-2 leading-none line-through font-lora text-xs sm:text-sm">
-                  {plan.coordinateNum}コーデ / {plan.itemNum}アイテム
-                </p>
-                <p className="flex items-center text-center text-xs">
-                  <span className="font-lora text-[7vw] sm:text-3xl mr-[1vw] sm:mr-1">
-                    {plan.coordinateNum}
-                  </span>
-                  コーデ
-                  <span className="mx-2">
-                    <img
-                      src="/images/icons/diagonal-line.svg"
-                      alt="diagonal-line"
-                      width="18vw"
-                    />
-                  </span>
-                  <span className="font-lora text-[7vw] sm:text-3xl mr-[1vw] sm:mr-1 text-red">
-                    {plan.itemNum + 1}
-                  </span>
-                  アイテム
-                </p>
-              </div>
-              <hr className="border border-dashed border-[#C8C9C3]" />
-              <div className="mx-auto py-4">
-                <p className="text-center mb-2 leading-none line-through font-lora text-xs sm:text-sm">
-                  トップス{plan.topsNum}+ボトムス{plan.bottomsNum}+ジャケット1
-                </p>
-                <p className="h-full flex items-center text-center text-xs sm:text-sm">
-                  トップス
-                  <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1 text-red">
-                    {plan.topsNum + 1}
-                  </span>
+          <div className="mx-auto py-4">
+            <p className="h-full flex items-center text-center text-xs">
+              <span className="font-lora text-[7vw] sm:text-3xl mr-[1vw] sm:mr-1">
+                {plan.coordinateNum}
+              </span>
+              コーデ
+              <span className="mx-2">
+                <img
+                  src="/images/icons/diagonal-line.svg"
+                  alt="diagonal-line"
+                  width="18vw"
+                />
+              </span>
+              <span className="font-lora text-[7vw] sm:text-3xl mr-[1vw] sm:mr-1">
+                {plan.itemNum}
+              </span>
+              アイテム
+            </p>
+          </div>
+          <hr className="border border-dashed border-[#C8C9C3]" />
+          <div className="mx-auto py-4">
+            <p className="h-full flex items-center text-center text-xs sm:text-sm">
+              トップス
+              <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1">
+                {plan.topsNum}
+              </span>
+              <span className="text-gray mx-1">＋</span>
+              ボトムス
+              <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1">
+                {plan.bottomsNum}
+              </span>
+              {plan.isJacketAvailable && (
+                <>
                   <span className="text-gray mx-1">＋</span>
-                  ボトムス
+                  <span className="flex flex-col text-xs text-left">
+                    ジャケット or
+                    <br />
+                    ライトアウター
+                  </span>
                   <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1">
-                    {plan.bottomsNum}
+                    1
                   </span>
-                  {plan.isJacketAvailable && (
-                    <>
-                      <span className="text-gray mx-1">＋</span>
-                      <span className="flex flex-col text-xs text-left">
-                        ジャケット or
-                        <br />
-                        ライトアウター
-                      </span>
-                      <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1">
-                        1
-                      </span>
-                    </>
-                  )}
-                </p>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="mx-auto py-4">
-                <p className="h-full flex items-center text-center text-xs">
-                  <span className="font-lora text-[7vw] sm:text-3xl mr-[1vw] sm:mr-1">
-                    {plan.coordinateNum}
-                  </span>
-                  コーデ
-                  <span className="mx-2">
-                    <img
-                      src="/images/icons/diagonal-line.svg"
-                      alt="diagonal-line"
-                      width="18vw"
-                    />
-                  </span>
-                  <span className="font-lora text-[7vw] sm:text-3xl mr-[1vw] sm:mr-1">
-                    {plan.itemNum}
-                  </span>
-                  アイテム
-                </p>
-              </div>
-              <hr className="border border-dashed border-[#C8C9C3]" />
-              <div className="mx-auto py-4">
-                <p className="h-full flex items-center text-center text-xs sm:text-sm">
-                  トップス
-                  <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1">
-                    {plan.topsNum}
-                  </span>
-                  <span className="text-gray mx-1">＋</span>
-                  ボトムス
-                  <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1">
-                    {plan.bottomsNum}
-                  </span>
-                  {plan.isJacketAvailable && (
-                    <>
-                      <span className="text-gray mx-1">＋</span>
-                      <span className="flex flex-col text-xs text-left">
-                        ジャケット or
-                        <br />
-                        ライトアウター
-                      </span>
-                      <span className="font-lora text-2xl mr-[1vw] sm:mr-1 ml-1">
-                        1
-                      </span>
-                    </>
-                  )}
-                </p>
-              </div>
-            </>
-          )}
+                </>
+              )}
+            </p>
+          </div>
         </div>
         <div className="bg-themeGray text-clay text-center text-[4vw] sm:text-xl py-1">
           ポイント
