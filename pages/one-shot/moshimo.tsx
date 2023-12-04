300d524a9bacaf4a5fa6ca4d19cddb1259d70b3b
Yuta Tsumori <yutatsumori@gmail.com>
2023-12-04 18:00:57 +0900

    fix: sessionStorage保存処理を3秒間待機するよう修正 <release/LEEAP-5058>

--- pages/one-shot/moshimo.tsx
+++ pages/one-shot/moshimo.tsx
@@ -4,13 +4,30 @@ import { useEffect } from "react";
 
 const Moshimo: NextPage = () => {
   const router = useRouter();
-  const { rd_code } = router.query;
+  const { one_shot_rd_code } = router.query;
+
   useEffect(() => {
-    if (rd_code && rd_code === "string") {
-      sessionStorage.setItem("one_shot_rd_code", rd_code);
-    }
-    router.push("https://one.uwear.jp/");
-  }, [rd_code, router]);
+    const storeAndRedirect = async () => {
+      if (typeof one_shot_rd_code === "string") {
+        sessionStorage.setItem("one_shot_rd_code", one_shot_rd_code);
+      }
+
+      // 保存前にリダイレクトすると保存されないので、保存されたことを確認するまで待つ
+      let attempts = 0;
+      const intervalId = setInterval(() => {
+        attempts++;
+        const item = sessionStorage.getItem("one_shot_rd_code");
+
+        if (item || attempts >= 3) {
+          clearInterval(intervalId);
+          router.push("https://one.uwear.jp/");
+        }
+      }, 1000);
+    };
+
+    storeAndRedirect();
+  }, [one_shot_rd_code, router]);
+
   return <></>;
 };
 
